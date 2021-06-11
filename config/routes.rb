1e841b72fb89665e228b9330f68bd8ea58c1f30f
032-shi <0324omitsu@gmail.com>
2021-06-11 10:27:32 +0000

    [add]マイページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :users
   resources :posts, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show]
 
 end
