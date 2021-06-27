a04ca8443a48852c71d24937e2d7a99f00db73cc
032-shi <0324omitsu@gmail.com>
2021-06-27 07:13:48 +0000

    [fix]デプロイの為、parts.scss内のurlをimage-urlに変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-# Rails.rootを使用するために必要。なぜなら、wheneverは読み込まれるときにrailsを起動する必要がある
+# wheneverは読み込まれるときにrailsを起動する必要がある。また、Rails.rootを使用するために必要。
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 # cronを実行する環境変数
 rails_env = ENV['RAILS_ENV'] || :development
@@ -8,6 +8,6 @@ set :environment, rails_env
 # cronのログの吐き出し場所。ここでエラー内容を確認する
 set :output, "#{Rails.root}/log/cron.log"
 
-every 1.minute do
+every 1.day, at: '1:15 pm' do #日本時間では+9時間になる
   runner "Batch::ScrapingBatch.scrapeBatch"
 end
