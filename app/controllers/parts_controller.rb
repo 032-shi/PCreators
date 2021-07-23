08595c670a962b5deb1b4a793e87aacd6ef69b97
032-shi <0324omitsu@gmail.com>
2021-07-23 06:11:03 +0000

    [finish]パーツ一覧画面のページングの非同期通信化実装

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -5,6 +5,9 @@ class PartsController < ApplicationController
   def narrowing
     @part_tag = PartTag.find(params[:part_tag_id])
     @parts = @part_tag.parts.page(params[:page]).per(20)
+    #以下、Ajax通信の場合のみ通過
+    return unless request.xhr?
+    render "parts/narrowing_pagenate"
   end
 
   def show
