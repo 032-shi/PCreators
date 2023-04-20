6b841cd764ba7f14b70daa70ec1ebc561655368a
032-shi <0324omitsu@gmail.com>
2023-04-20 15:14:15 +0900

    [fix]パーツTOP画面のリンクidの修正実施

--- app/models/part_tag.rb
+++ app/models/part_tag.rb
@@ -1,4 +1,15 @@
 class PartTag < ApplicationRecord
   has_many :part_tag_maps, dependent: :destroy, foreign_key: 'part_tag_id'
   has_many :parts, through: :part_tag_maps
+
+  # PCパーツ一覧TOP画面(/parts/index)のパーツタグ名を宣言する
+  CPU = 'CPU'
+  MEMORY = 'メモリー'
+  VIDEO_CARD = 'グラフィックボード'
+  MOTHER_BOARD = 'マザーボード'
+  CPU_COOLER = 'CPUクーラー'
+  PC_CASE = 'PCケース'
+  POWER_SUPPLY = 'PC電源'
+  SSD = 'SSD'
+  HDD = 'HDD'
 end
