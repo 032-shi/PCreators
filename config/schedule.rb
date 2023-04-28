fccb2892a64833644799ab71425926370d64a5e9
032-shi <0324omitsu@gmail.com>
2023-04-29 02:14:36 +0900

    [fix]バッチ起動設定の修正実施

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,19 @@
 # wheneverは読み込まれるときにrailsを起動する必要がある。また、Rails.rootを使用するために必要。
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-# cronを実行する環境変数
+# Railsアプリの実行環境を指定する環境変数をセットする
 rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
+# cronを実行する際の環境変数をセットする
 set :environment, rails_env
 
 # cronのログの吐き出し場所。ここでエラー内容を確認する
 set :output, "#{Rails.root}/log/cron.log"
 
-every 1.day, at: '1:45 pm' do #日本時間では+9時間になる
+# システム(アプリがデプロイされているマシン)の環境変数をセットする
+set :path_env, ENV['PATH']
+# cronにて実行する前に下記のjob_typeに従ってコマンドを変換して実行する(デフォルトだとシステム準拠のRubyが使用され、アプリのRubyと差異が発生したため、この変換処理を追加)
+job_type :runner, "cd :path && PATH=':path_env' bin/rails runner -e :environment ':task' :output"
+
+# 毎日、AM1:00にバッチを実行する
+every 1.day, at: '1:00 am' do
   runner "Batch::ScrapingBatch.scrapeBatch"
 end
--- lib/batch/scraping_batch.rb
+++ lib/batch/scraping_batch.rb
@@ -1,13 +1,15 @@
 class Batch::ScrapingBatch
   def self.scrapeBatch
     @logger = Logger.new('log/batch.log')
-    time = Time.current
-    @logger.info('scrapeBatch実行開始 ' + time.strftime("%Y-%m-%d %H:%M:%S"))
+    @logger.info('scrapeBatch実行開始 ' + Time.current.strftime("%Y-%m-%d %H:%M:%S"))
     begin
       Part.part_scrape
-      @logger.info('scrapeBatch実行完了 ' + time.strftime("%Y-%m-%d %H:%M:%S"))
+      @logger.info('scrapeBatch実行完了 ' + Time.current.strftime("%Y-%m-%d %H:%M:%S"))
     rescue => e
       @logger.error(e)
     end
+  end
+  def self.test
+    puts 'dadena'
   end
 end
